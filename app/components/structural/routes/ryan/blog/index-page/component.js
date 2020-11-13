61956a576bbb66e0ff4b6704aecdf6ebacfaf7f2
Ryan Roderick <ryan@designmonster.com>
2020-11-12 21:01:16 -0500

    nov11th

--- app/components/structural/routes/ryan/blog/index-page/component.js
+++ app/components/structural/routes/ryan/blog/index-page/component.js
@@ -17,4 +17,14 @@ export default class StructuralRoutesRyanBlogIndexPageComponent extends Componen
   headerTitle = 'Web Developer';
   headerButtonLabel = 'Hire Me!';
   headerButtonRoute = 'ryan.contact';
+  emmTitle = 'Ember-Mobile-Menu For Beginners';
+  emmDate = '12/01/2020';
+  emmDescription = 'A write up for our journey to get Ember Mobile Menu Working on our site.  We go in depth on what we did wrong, assumptions we made, and how we fixed those problems to get it up and running.'
+  emmButtonRoute = 'ryan.blog.ember-mobile-menu';
+  emmButtonLabel = 'Read More';
+  gfbTitle = 'Git For Beginners';
+  gfbDate = '12/01/2020';
+  gfbDescription = 'We all Struggle With GIT, especially when you\'re just starting out.  We\'ve laid out some basic git commands that all beginners should know.'
+  gfbButtonRoute = 'ryan.blog.git-for-beginners';
+  gfbButtonLabel = 'Read More';
 }
