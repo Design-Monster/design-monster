c3b6f97a10a898f558f224d64c7777cd5830b0a3
Ryan Roderick <ryan@designmonster.com>
2020-11-25 08:33:22 -0500

    final version built

---
+++ app/helpers/isRootUrl.js
@@ -0,0 +1,15 @@
+import Helper from "@ember/component/helper";
+import { inject as service } from '@ember/service';
+
+export default class isRootUrl extends Helper {
+  @service router;
+
+  compute() {
+    let rootURL = this.router.rootURL;
+    let currentURL = this.router.currentURL
+
+    if (currentURL === rootURL) {
+      return true;
+    }
+  }
+}
